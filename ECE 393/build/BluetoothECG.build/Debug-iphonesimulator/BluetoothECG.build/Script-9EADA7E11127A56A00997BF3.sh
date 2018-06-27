6162e66580525a471b34c87edb7c924569fb4386
quyphamdev <40367214+quyphamdev@users.noreply.github.com>
2018-06-27 00:36:37 -0400

    Remove ECE 393 folder

--- ECE 393/build/BluetoothECG.build/Debug-iphonesimulator/BluetoothECG.build/Script-9EADA7E11127A56A00997BF3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export CODESIGN_ALLOCATE=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/codesign_allocate
-if [ "${PLATFORM_NAME}" == "iphoneos" ]; then
-	/Users/jasonmac/Desktop/gen_entitlements.py "my.company.${PROJECT_NAME}" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/${PROJECT_NAME}.xcent";
-	codesign -f -s "iPhone Developers" --resource-rules "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/ResourceRules.plist" \
-		 --entitlements "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/${PROJECT_NAME}.xcent"  "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/"
-fi
-
